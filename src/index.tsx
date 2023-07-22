afadc4ccf2cb1c80ac56b3b1a76b716e76e4fdd9
Pedro Rojas <pedrofrojasu@gmail.com>
2023-07-23 00:56:31 +0200

    enabled devtools

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { render } from "solid-js/web"
 import "./index.css"
 import App from "./App"
 import { Router } from "@solidjs/router"
+import "solid-devtools"
 
 const root = document.getElementById("root")
 
